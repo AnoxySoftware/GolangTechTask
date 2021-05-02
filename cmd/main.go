cbe7549527acf8be7ecef6f5b66f4f209bdbf2d0
Ben Woodward <ben@benwoodward.me.uk>
2021-05-02 22:38:07 +0100

    Add basic server implementation with listvotables

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"net"
+	"os"
+	"os/signal"
+
+	"github.com/Sacro/GolangTechTask/internal/server"
+	"github.com/apex/log"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/guregu/dynamo"
+	"google.golang.org/grpc"
+
+	pb "github.com/Sacro/GolangTechTask/api"
+)
+
+const (
+	endpoint = "http://127.0.0.1:8000"
+	region   = "region"
+	id       = "id"
+	secret   = "secret"
+	token    = "token"
+	port     = ":3000"
+)
 
 func main() {
-	fmt.Println("hello world")
+	session, err := session.NewSession()
+	if err != nil {
+		log.WithError(err).Fatal("creating session")
+	}
+
+	s := grpc.NewServer()
+
+	config := aws.NewConfig().WithEndpoint(endpoint).WithRegion(region).WithCredentials(credentials.NewStaticCredentials(id, secret, token))
+	db := dynamo.New(session, config)
+	srv := server.NewDynamodbServer(db)
+
+	pb.RegisterVotingServiceServer(s, srv)
+
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		log.WithError(err).Fatal("establishing listener")
+	}
+
+	stopChan := make(chan os.Signal, 1)
+	errChan := make(chan error)
+
+	// bind OS events to signal channel
+	signal.Notify(stopChan, os.Interrupt)
+
+	// run blocking call in a separate goroutine, report errors via channel
+	go func() {
+		if err := s.Serve(listener); err != nil {
+			errChan <- err
+		}
+	}()
+
+	defer func() {
+		s.GracefulStop()
+	}()
+
+	select {
+	case err := <-errChan:
+		log.WithError(err).Fatal("running server")
+	case <-stopChan:
+		log.Info("stopping server")
+	}
 }
--- internal/server/dynamodb_server.go
+++ internal/server/dynamodb_server.go
@@ -4,10 +4,35 @@ import (
 	"context"
 
 	"github.com/Sacro/GolangTechTask/api"
+	"github.com/Sacro/GolangTechTask/internal/models"
+	"github.com/apex/log"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/guregu/dynamo"
 )
+
+const tableName = "Voteables"
 
 type DynamodbServer struct {
 	api.UnimplementedVotingServiceServer
+
+	table dynamo.Table
+}
+
+func NewDynamodbServer(db *dynamo.DB) *DynamodbServer {
+	err := db.CreateTable(tableName, models.Voteable{}).Run()
+	if err != nil {
+		// If ResourceInUseException is thrown our table already exists
+		if err.(awserr.Error).Code() != "ResourceInUseException" {
+
+			log.WithError(err).Info("creating table")
+		}
+	}
+
+	table := db.Table(tableName)
+
+	return &DynamodbServer{
+		table: table,
+	}
 }
 
 func (s DynamodbServer) CastVote(ctx context.Context, r *api.CastVoteRequest) (*api.CastVoteResponse, error) {
@@ -19,5 +44,30 @@ func (s DynamodbServer) CreateVoteable(ctx context.Context, r *api.CreateVoteabl
 }
 
 func (s DynamodbServer) ListVoteables(context.Context, *api.ListVoteablesRequest) (*api.ListVoteablesResponse, error) {
-	panic("not implemented")
+	var results []models.Voteable
+	err := s.table.Scan().All(&results)
+	if err != nil {
+		return nil, err
+	}
+
+	var voteables []*api.Voteable
+
+	for _, r := range results {
+		// Convert answers map to slice
+		var answers = make([]string, len(r.Answers))
+		for _, a := range r.Answers {
+			answers = append(answers, a)
+		}
+
+		voteables = append(voteables, &api.Voteable{
+			Uuid:     r.UUID,
+			Question: r.Question,
+			Answers:  answers,
+		})
+	}
+
+	return &api.ListVoteablesResponse{
+		Votables: voteables,
+	}, nil
+
 }
